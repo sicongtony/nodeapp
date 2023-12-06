9e2097d56111b2ec43af574e4cfccc4aa32049a1
sicongtony <sscpromise@gmail.com>
2023-12-06 14:16:10 +1100

    1 ignore all node_modukes 2 Use mongoose 3 update code about CRUD functions

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const errorController = require('./controllers/error');
-const mongoConnect = require('./util/database').mongoConnect;
 const User = require('./models/user');
 
 const app = express();
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res, next) => {
   User.findById('656ee8736037bd839e899c61')
     .then(user => {
-      req.user = new User(user.name, user.email, user.cart, user._id);
+      req.user = user;
       next();
     })
     .catch(err => console.log(err));
@@ -32,6 +32,14 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect(() => {
-  app.listen(3000);
-});
+mongoose
+  .connect(
+    'mongodb+srv://tonyshen1:Ho3W8opKOXpFoXe3@cluster0.zpkp9du.mongodb.net/shop?retryWrites=true'
+  )
+  .then(result => {
+    console.log("Successfully connected to Atlas");
+    app.listen(3000);
+  })
+  .catch(err => {
+    console.log(err);
+  });
